da527e20493c059334285686f4c34aaf017288d0
Diego <163057208+Saintdiegx@users.noreply.github.com>
2025-07-15 19:47:56 -0500

    correcciones p2

--- src/lab8/PanelPrincipal.java
+++ src/lab8/PanelPrincipal.java
@@ -5,13 +5,12 @@ import lab8.Problema2.PanelPalindromos;
 import javax.swing.*;
 
 public class PanelPrincipal {
-    private JPanel PanelPrincipal; // Panel raíz del formulario .form
-    private JButton buttonProblema2; // ← Nombre actualizado del botón
-    private JButton p1Button;
+    private JPanel PanelPrincipal;
+    private JButton buttonProblema2;
+    private JButton buttonProblema1;
 
     public PanelPrincipal(JFrame frame) {
         buttonProblema2.addActionListener(e -> {
-            // Cambiar al panel del Problema 2 (PanelPalindromos)
             PanelPalindromos panelPalindromos = new PanelPalindromos();
             frame.setContentPane(panelPalindromos.getPanel());
             frame.revalidate();
